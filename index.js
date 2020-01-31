e612730c318549aefa363c67aeeb9c65778c13dd
ColinLi33 <Colin8tor1206@gmail.com>
2020-01-31 17:37:08 -0600

    fixed heroku stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'))
-app.get('/', (req, res) => res.render('index.html'))
+app.get('/', function(req, res) {
+	res.render(__dirname + './public/index.html');
+});
 
 app.listen(process.env.PORT || 3333, () => console.log('Hosting on 3333'))
