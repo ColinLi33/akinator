4a28bb1a12fff3ef46f18df4f3a16dfc5d56bf03
ColinLi33 <Colin8tor1206@gmail.com>
2020-01-31 17:51:02 -0600

    trying to get heroku to work

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = require('express')();
-const http = require('http').createServer(app);
 app.use(express.static('public'));
-app.get('/', function(req, res) {
-	res.render(__dirname + './public/index.html');
-  res.render(__dirname + './public/twentyQ.js');
-});
 
-http.listen(process.env.PORT || 3333, function() {
+app.listen(process.env.PORT || 3333, function() {
 	console.log('listening on 3333');
 });
