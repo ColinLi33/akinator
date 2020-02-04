1664ed894fa337c832085fa1f58bb8951f46f76c
Colin Li <213003@NA200-A001465.local>
2020-02-04 14:15:02 -0600

    questions

--- public/twentyQ.js
+++ public/twentyQ.js
@@ -1,22 +1,5 @@
-var arr = [
-    ['Alabama', 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1], ['Alaska', 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0], ['Arizona', 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0], ['Arkansas', 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], ['California', 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-    ['Colorado', 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0], ['Connecticut', 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1], ['Delaware', 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1], ['Florida', 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1], ['Georgia', 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1],
-    ['Hawaii', 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0], ['Idaho', 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0], ['Illinois', 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1], ['Indiana', 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1], ['Iowa', 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0], ['Kansas', 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    ['Kentucky', 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1], ['Louisiana', 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], ['Maine', 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], ['Maryland', 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1], ['Massachusetts', 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    ['Michigan', 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1], ['Minnesota', 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0], ['Mississippi', 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1], ['Missouri', 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0], ['Montana', 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0],
-    ['Nebraska', 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], ['Nevada', 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0], ['New Hampshire', 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1], ['New Jersey', 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1],
-    ['New Mexico', 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0], ['New York', 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1], ['North Carolina', 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1], ['North Dakota', 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1],
-    ['Ohio', 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1], ['Oklahoma', 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0], ['Oregon', 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0], ['Pennsylvania', 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1], ['Rhode Island', 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-    ['South Carolina', 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1], ['South Dakota', 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0], ['Tennessee', 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1], ['Texas', 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0], ['Utah', 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-    ['Vermont', 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1], ['Virginia', 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1], ['Washington', 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0], ['West Virginia', 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1], ['Wisconsin', 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    ['Wyoming', 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-];
-
-let questions = ['Is your state landlocked?', 'Does your state have a Democratic governor?', 'Is the population of your state greater than 5 million?',
-	"Is the length your state's name less than 8 letters?", "Does your state border Canada?", 'Does the name of your state have 2 words?',
-	'Does your state start with a vowel?', 'Does your state end with a vowel (Y is not a vowel)?', 'Does your state border the Atlantic Ocean?',
-	'Does your state border a Great Lake?', 'Is your state east of the Mississippi River?'
-];
+let arr = arrInfo;
+let questions = questionsInfo;
 let currQuest = 0;
 
 let yesButton = document.getElementById("yesButton");
